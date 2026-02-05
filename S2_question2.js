f142e4fabab6723a56ee371ffca8e546384af1e3
Ramendra <thegentlemonk@gmail.com>
2026-02-05 15:58:21 +0530

    Add rate limiter utility using closures and timing
    Implemented a rate limiter utility that allows a function to execute only once within a specified delay. Additional calls during the delay are ignored.

---
+++ S2_question2.js
@@ -0,0 +1,32 @@
+// Question 2: Rate Limiter (Closures + Timing) 
+// Create a utility: const limitedFn = rateLimit(fn, delay); 
+// Behavior: The function should execute only once within the given delay. 
+// Extra calls during the delay should be ignored. Example: 
+// limitedFn(); // runs 
+// limitedFn(); // ignored 
+// Notes: Must use closures. Use setTimeout or similar timing logic.
+
+
+function rateLimit(fn, delay) {
+  let canRun = true
+
+  return function () {
+    if (!canRun) return
+
+    canRun = false
+    fn()
+
+    setTimeout(() => {
+      canRun = true
+    }, delay)
+  }
+}
+
+const limitedFn = rateLimit(() => {
+  console.log("function executed")
+}, 2000)
+
+limitedFn()
+
+//I’m using a closure to store a flag that controls execution and resetting it after the delay using setTimeout.
+limitedFn()
